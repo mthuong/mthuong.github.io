770c7b25cb2aa3279d0de2e4ebf34b6933f074d5
Tom <thuong.nguyen@itads.net>
2021-09-29 15:44:04 +0700

    feat: fix prettier

---
+++ precache-manifest.3f5e6c066872d70f20b1fb0a22ab1abb.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f85d26e1620a01ec37b66fb235ac29a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4e5d4354d8ec021fb1c",
+    "url": "/static/css/3.6e2b7420.chunk.css"
+  },
+  {
+    "revision": "c2f7407cc8e7922f651f",
+    "url": "/static/css/4.e0667186.chunk.css"
+  },
+  {
+    "revision": "47a7f9d0ab5ff67e8e87",
+    "url": "/static/css/main.6cb9285a.chunk.css"
+  },
+  {
+    "revision": "079e80b3c410befc4256",
+    "url": "/static/js/2.7f85a02c.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.7f85a02c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4e5d4354d8ec021fb1c",
+    "url": "/static/js/3.427651c5.chunk.js"
+  },
+  {
+    "revision": "c2f7407cc8e7922f651f",
+    "url": "/static/js/4.5b20c22c.chunk.js"
+  },
+  {
+    "revision": "47a7f9d0ab5ff67e8e87",
+    "url": "/static/js/main.b2c5f71e.chunk.js"
+  },
+  {
+    "revision": "cd887432f700d6d524ed",
+    "url": "/static/js/runtime-main.80839c50.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "0576996e70b5d1e5f49ae2f63373fc1d",
+    "url": "/static/media/hcmute.0576996e.png"
+  },
+  {
+    "revision": "68dbcdf8feb060bc6c19b957b0dab7bf",
+    "url": "/static/media/itads.68dbcdf8.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "265e44d3ea17280ca6af7843ca5f2d0d",
+    "url": "/static/media/selenium.265e44d3.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "51f246755abe52421b8c32971a7e731b",
+    "url": "/static/media/swift.51f24675.png"
+  },
+  {
+    "revision": "af0095aa7baea8901de3e1b52d3d9680",
+    "url": "/static/media/swiftui.af0095aa.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "40683952719c7953e048e4811cfadc1b",
+    "url": "/static/media/tma.40683952.jpeg"
+  },
+  {
+    "revision": "9f8ab0c713ab89c6eac4dd3bd300914b",
+    "url": "/static/media/vinasource.9f8ab0c7.png"
+  },
+  {
+    "revision": "007d3bc3426362d438a44f105ea61fae",
+    "url": "/static/media/xcode.007d3bc3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5d6ca57192eeaa0f751b450011cb9c19.js"
+  "/precache-manifest.3f5e6c066872d70f20b1fb0a22ab1abb.js"
 );
 
 self.addEventListener('message', (event) => {
